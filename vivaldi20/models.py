55e1f49ce42cbfcd6314403c73e7fa379c9e0539
Nicholas Ngetich <ngetichnicholas903@gmail.com>
2024-10-14 22:51:18 +0300

    add user model

--- vivaldi20/models.py
+++ vivaldi20/models.py
@@ -1,3 +1,10 @@
+from django.contrib.auth.models import AbstractUser
 from django.db import models
 
-# Create your models here.
+class User(AbstractUser):
+    profession = models.CharField(max_length=100, default="AWS Cloud Practitioner")
+    bio = models.TextField(blank=True, default="No bio provided")
+    profile_photo = models.ImageField(upload_to='profile_photos/', blank=True, null=True)
+
+    def __str__(self):
+        return self.username
